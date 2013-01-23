bf6f17a4043d0653d9ee1309fa594afe2e40e7d6
agnosco11@gmail.com <agnosco11@gmail.com@00d2af04-9d7a-2a42-4159-160411b49c3b>
2013-01-23 11:32:33 +0000

    final database design for online exam system

--- trunk/project/onlineexam.sql
+++ trunk/project/onlineexam.sql
@@ -31,6 +31,74 @@ ENGINE = MyISAM;
 CREATE  TABLE IF NOT EXISTS `onlineexam`.`user_type` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NOT NULL ,
+  `slug` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `onlineexam`.`courses`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `onlineexam`.`courses` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `description` VARCHAR(200) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `onlineexam`.`questions`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `onlineexam`.`questions` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `exam_id` INT NOT NULL ,
+  `questiontype_id` INT NOT NULL ,
+  `remark` VARCHAR(200) NULL ,
+  `marks` INT NOT NULL ,
+  `answer` VARCHAR(200) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `onlineexam`.`answers`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `onlineexam`.`answers` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `question_id` INT NOT NULL ,
+  `user_id` INT NOT NULL ,
+  `marksobtained` INT NOT NULL ,
+  `answer` VARCHAR(200) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `onlineexam`.`exam`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `onlineexam`.`exam` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `description` VARCHAR(200) NULL ,
+  `full_marks` INT NOT NULL ,
+  `pass_marks` INT NOT NULL ,
+  `start_time` INT NOT NULL ,
+  `end_time` INT NOT NULL ,
+  `date` INT NULL ,
+  `created_at` INT NULL ,
+  `course_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `onlineexam`.`question_type`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `onlineexam`.`question_type` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `slug` VARCHAR(100) NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = MyISAM;
 
