5b5336b9df6f0323472fa75085956ba122188888
agnosco11@gmail.com <agnosco11@gmail.com@00d2af04-9d7a-2a42-4159-160411b49c3b>
2013-01-30 10:44:27 +0000

    added pages for backend

---
+++ trunk/project/design/back_end/add_courses.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Courses</title>
+    
+    <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="assets/css/docs.css" rel="stylesheet">
+    <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+         <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+  </head>
+<header class="jumbotron subhead" id="overview">
+  <div class="container">
+      <form method="post" action="<?php echo "courses.php"?>">
+      <input type="submit" value="ADDNext">
+      <button type="button" onClick="edit_course();">Update</button>
+      <button type="button" onClick="cancel_user();">Cancel</button>
+      <button type="button" onClick="finish_user();">Finish</button>
+       
+        <label>Course Name:</label><input rdtype="text" name="coursename" value="" />
+        <br/>
+         <label>Description:</label>
+        <input type="text" name="description" value="" />
+        <br/>
+        
+    </form>
+
+ </div>
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+    <script src="assets/js/bootstrap-affix.js"></script>
+
+    <script src="assets/js/holder/holder.js"></script>
+    <script src="assets/js/google-code-prettify/prettify.js"></script>
+
+    <script src="assets/js/application.js"></script>
+
+
+
+  </body>
+</html>
---
+++ trunk/project/design/back_end/courses.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Online Exam System</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="assets/css/docs.css" rel="stylesheet">
+    <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/jquery.lightbox-0.5.css" media="screen" />
+	    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".bs-docs-sidebar">
+
+    <!-- Navbar
+    ================================================== -->
+    
+<!-- Subhead
+================================================== -->
+<header class="jumbotron subhead" id="overview">
+  <div class="container">
+    <h1>Online Examination System</h1>
+    <p class="lead">ADD/UPDATE</p>
+  </div>
+</header>
+<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="./index.html">Bootstrap</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="">
+                <a href="./index.php">Users</a>
+              </li>
+              <li class="">
+                <a href="./courses.php">Courses</a>
+              </li>
+              <li class="">
+                <a href="./questions.php">Questions</a>
+              </li>
+              <li class="">
+                <a href="./answers.php">Answers</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  <div class="container">
+  <table border="2" width="1200">
+	<thead>
+      <tr>
+	<th width="300">Course Name</th>
+	<th>Description</th>
+	</tr>
+	</thead>	           
+	<tbody>
+<?php
+$count=0;
+$con = mysql_connect('localhost', 'root', '');
+				if (!$con) {
+					die('Could not connect: ' . mysql_error());
+					
+				} 
+				else
+				{
+				
+				mysql_select_db("onlineexam", $con);
+				$sel_course= $_POST["coursename"];
+				$sel_desc = $_POST["description"];
+				
+				mysql_query	("INSERT INTO `onlineexam`.`courses` (`id`, `name`, `description`) VALUES (NULL, '$sel_course', '$sel_desc')");				
+				$sql = "select * From courses";
+				
+							
+				
+		$result = mysql_query($sql);
+				
+
+			while(($row = mysql_fetch_array($result))!=0)
+				  {
+					
+				  echo "<tr><td>";
+					echo $row['name']."</td>";
+					echo "<td>".$row['description']."</td>";
+				 echo"</tr>";		
+					$count++;
+			}
+			
+			if($count == 0){
+			echo "<tr><td colspan = \"2\" align=\"center\">";
+				echo "no records available";
+			echo"</td></tr>";
+			}
+			
+			
+			}
+			?>
+			</tbody>
+			</table>
+          <a href="./add_courses.php"><button type="button">ADD New Course</button></a>
+  </div>
+
+
+
+    <!-- Footer
+    ================================================== -->
+    <footer class="footer">
+      <div class="container">
+        
+       </div>
+    </footer>
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script src="assets/js/jquery.js"></script>
+    <script type="assets/javascript" src="js/jquery.lightbox-0.5.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+    <script src="assets/js/bootstrap-affix.js"></script>
+
+    <script src="assets/js/holder/holder.js"></script>
+    <script src="assets/js/google-code-prettify/prettify.js"></script>
+
+    <script src="assets/js/application.js"></script>
+<script>
+$(function() {
+        $('#gallery a').lightBox();
+    });
+</script>
+
+
+  </body>
+</html>
